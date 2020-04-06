a3f687e83d0da7670004eecd29d89b165a6e3aac
NoelleP <61802909+NoelleP@users.noreply.github.com>
2020-04-06 13:30:35 -0700

    added stuff, still not working

---
+++ Cat/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw (){
+  Cat();
+}
+
+class Cat {
+// constructor(x,y){
+//   this.x = x;
+//   this.y = y;
+//   }
+
+drawCat(){
+  ellipse(100,100,10,10);
+  }
+
+// moveCat(){
+//   this.x
+// }
+// }
