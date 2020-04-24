deabc0708a7cd78abdd9549efaa8d4573e5c70d8
NoelleP <61802909+NoelleP@users.noreply.github.com>
2020-04-24 12:07:55 -0700

    changed cat direction, added keira code

--- Cat/3-conditional-statement/sketch.js
+++ Cat/3-conditional-statement/sketch.js
@@ -1,21 +1,25 @@
 //create an empty array called balls
 let cats = [];
+let me;
 
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(400, 500);
+  me = new Avatar(width/2, 300, 3);
 }
 
 function draw(){
 	background(220);
+  me.drawMe();
+  me.moveMe();
 
-  if (frameCount % 25 == 0) {
-      let  c = new Cat(width, random(0,height), -3);
+  if (frameCount % 55 == 0) {
+      let  c = new Cat(random(0,width), height-500, -3);
       cats.push(c);
       console.log(cats); //print the balls array to the console
     }
 
-//	draw all the balls in that array
+	// draw all the balls in that array
 	for (let i = 0; i < cats.length; i++) {
 	 	      cats[i].drawCat();
        	  cats[i].moveCat();
@@ -34,42 +38,119 @@ class Cat {
 
 	// draw a ball on the screen at x,y
 	drawCat(){
+    //whiskers
     push();
     stroke(138, 101, 37);
     strokeWeight(1);
-    line(this.x-25,this.y-8,this.x-27,this.y-13);
-    line(this.x-23,this.y-8,this.x-23,this.y-13);
-    line(this.x-21,this.y-8,this.x-19,this.y-13);
-    line(this.x-25,this.y+8,this.x-27,this.y+13);
-    line(this.x-23,this.y+8,this.x-23,this.y+13);
-    line(this.x-21,this.y+8,this.x-19,this.y+13);
+    line(this.x-8,this.y-25+42,this.x-13,this.y-27+42);
+    line(this.x-8,this.y-23+42,this.x-13,this.y-23+42);
+    line(this.x-8,this.y-21+42,this.x-13,this.y-19+42);
+    line(this.x+8,this.y-25+42,this.x+13,this.y-27+42);
+    line(this.x+8,this.y-23+42,this.x+13,this.y-23+42);
+    line(this.x+8,this.y-21+42,this.x+13,this.y-19+42);
     pop();
 
+//head + body
       noStroke();
     	fill(232, 191, 109);
-		  ellipse(this.x,this.y,35,25);
-      ellipse(this.x-20,this.y,18,18);
+		  ellipse(this.x,this.y,25,35);
+      ellipse(this.x,this.y+20,18,18);
 
+//tail
       push();
       stroke(232, 191, 109);
       noFill();
       strokeWeight(8);
-      translate(-5,-45);
-      bezier(this.x+119/6, this.y+286/6, this.x+294/6, this.y+264/6, this.x+138/6, this.y+157/6, this.x+342/6, this.y+175/6);
+      translate(-30,-65);
+      bezier(this.x+286/6, this.y+119/6, this.x+264/6, this.y+294/6, this.x+157/6, this.y+138/6, this.x+175/6, this.y+342/6);
       pop();
 
+//eyes
       push();
       noStroke();
       fill(138, 101, 37);
-      ellipse(this.x-23,this.y-3,4,4);
-      ellipse(this.x-23,this.y+3,4,4);
+      ellipse(this.x-4,this.y+23,4,4);
+      ellipse(this.x+3,this.y+23,4,4);
       pop();
 	}
 
 	//update the location of the ball, so it moves across the screen
 	moveCat(){
-    this.x = this.x+ this.speed;
-		this.y = this.y+.5;
+    this.x = this.x;
+		this.y = this.y- this.speed;
 	}
+
+}
+
+class Avatar {
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+	drawMe(){  // draw the running person
+    		noStroke();
+        fill(143, 199, 70);
+        //arms
+        ellipse(this.x+20,this.y-12,20, 10);
+        ellipse(this.x+20,this.y+12,20, 10);
+        ellipse(this.x-20,this.y-12,20, 10);
+        ellipse(this.x-20,this.y+12,20, 10);
+        //head
+        ellipse(this.x, this.y-25, 20,25);
+        //tail
+        triangle(this.x, this.y+32, this.x+15, this.y, this.x-15, this.y);
+        //shell
+        fill(45,143,45);
+		    ellipse(this.x,this.y,40,50);
+        //eyes
+        fill("black");
+        ellipse(this.x+8,this.y-30,4,4);
+        ellipse(this.x-8,this.y-30,4,4);
+        //shell design
+        stroke(102,102,51);
+        strokeWeight(1);
+        line(this.x-9,this.y-3.75, this.x-3.75, this.y-11.25);
+        line(this.x+9,this.y+3.75, this.x+3.75, this.y+11.25);
+        line(this.x+9,this.y-3.75, this.x+3.75, this.y-11.25);
+        line(this.x-9,this.y+3.75, this.x-3.75, this.y+11.25);
+        line(this.x-3.75, this.y+11.25, this.x+3.75, this.y+11.25)
+        line(this.x-3.75, this.y-11.25, this.x+3.75, this.y-11.25)
+        line(this.x-9, this.y-3.75, this.x-9, this.y+3.75)
+        line(this.x+9, this.y-3.75, this.x+9, this.y+3.75)
+        line(this.x+9, this.y-3.75, this.x+18, this.y-8)
+        line(this.x-9, this.y-3.75, this.x-18, this.y-8)
+        line(this.x-3.75, this.y-11.25, this.x-7, this.y-23)
+        line(this.x-3.75, this.y+11.25, this.x-7, this.y+23)
+        line(this.x+3.75, this.y-11.25, this.x+7, this.y-23)
+        line(this.x+3.75, this.y+11.25, this.x+7, this.y+23)
+        line(this.x+9, this.y+3.75, this.x+18, this.y+8)
+        line(this.x-9, this.y+3.75, this.x-18, this.y+8)
+
+	}
+
+	moveMe(){
+    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+        this.y += this.speed;
+      }
+        if(keyIsDown(LEFT_ARROW)){
+     this.x -=this.speed;
+   }
+
+   if(keyIsDown(RIGHT_ARROW)){
+     this.x +=this.speed;
+
+    }
+	}
+
+  die(){
+
+  }
 
 }
