8c4351928de15d997b7f8c2775edbe2635afc407
keiranakamura <61802876+keiranakamura@users.noreply.github.com>
2020-04-29 12:13:49 -0700

    add

--- Game-Project/tortoid/sketch.js
+++ Game-Project/tortoid/sketch.js
@@ -1,5 +1,5 @@
 //create an empty array called balls
-// let balls = [];
+let balls = [];
 
 //create a variable to hold your avatar
 let me;
@@ -29,18 +29,18 @@ function draw(){
   me.drawMe();
   me.moveMe();
 
-//   if (frameCount % 100 == 0) {
-//       let  b = new Ball(width, random(0,height), -3);
-//       balls.push(b);
-//       console.log(balls); //print the balls array to the console
-//     }
-//
-// //	draw all the balls in that array
-// 	for (let i = 0; i < balls.length; i++) {
-// 	 	      balls[i].drawBall();
-//        	  balls[i].moveBall();
-//         	balls[i].bounceBall();
-// 	  }
+  if (frameCount % 100 == 0) {
+      let  b = new Ball(width, random(0,height), -3);
+      balls.push(b);
+      console.log(balls); //print the balls array to the console
+    }
+
+//	draw all the balls in that array
+	for (let i = 0; i < balls.length; i++) {
+	 	      balls[i].drawBall();
+       	  balls[i].moveBall();
+        	balls[i].bounceBall();
+	  }
 
 }
 
@@ -119,43 +119,43 @@ class Avatar {
 }
 
 
-// //ball class from which to create new balls with similar properties.
-// class Ball {
-//
-// 	//every ball needs an x value, a y value, and a speed
-// 	constructor(x,y, speed){
-// 		this.x = x;
-//     this.y = y;
-//     this.speed = speed;
-// 	}
+//ball class from which to create new balls with similar properties.
+class Ball {
 
-// 	// draw a ball on the screen at x,y
-// 	drawBall(){
-//     	noStroke();
-//       // strokeWeight(1);
-//     	fill(200,150, 150);
-// 		  ellipse(this.x,this.y,20,18);
-//       fill(220, 130, 130)
-//       ellipse(this.x+30,this.y,40,15);
-//       triangle(this.x+40, this.y+20, this.x+12, this.y-2, this.x+30, this.y-5 )
-//       fill("orange")
-//       triangle(this.x-10, this.y+2, this.x-22, this.y, this.x-10, this.y-2)
-//
-// 	}
-//
-// 	//update the location of the ball, so it moves across the screen
-// 	moveBall(){
-// 		this.x = this.x+ this.speed;
-// 		this.y = this.y+.5;
-// 	}
-//
-// 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-//   	bounceBall(){
-//     		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-//       			this.speed = -this.speed;
-//             mySound.setVolume(0.1);
-//             mySound.play();
-//     		}
-//   	}
-//
-// }
+	//every ball needs an x value, a y value, and a speed
+	constructor(x,y, speed){
+		this.x = x;
+    this.y = y;
+    this.speed = speed;
+	}
+
+	// draw a ball on the screen at x,y
+	drawBall(){
+    	noStroke();
+      // strokeWeight(1);
+    	fill(200,150, 150);
+		  ellipse(this.x,this.y,20,18);
+      fill(220, 130, 130)
+      ellipse(this.x+30,this.y,40,15);
+      triangle(this.x+40, this.y+20, this.x+12, this.y-2, this.x+30, this.y-5 )
+      fill("orange")
+      triangle(this.x-10, this.y+2, this.x-22, this.y, this.x-10, this.y-2)
+
+	}
+
+	//update the location of the ball, so it moves across the screen
+	moveBall(){
+		this.x = this.x+ this.speed;
+		this.y = this.y+.5;
+	}
+
+if the ball hits the person, change the speed value to negative (send it in the opposite direction)
+  	bounceBall(){
+    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+      			this.speed = -this.speed;
+            mySound.setVolume(0.1);
+            mySound.play();
+    		}
+  	}
+
+}
