fc508968ded033838d6824ec7b306486eb52064f
NoelleP <61802909+NoelleP@users.noreply.github.com>
2020-05-04 12:04:48 -0700

    added calico cats

--- Game-Project/3-conditional-statement/sketch.js
+++ Game-Project/3-conditional-statement/sketch.js
@@ -1,97 +1,44 @@
 //create an empty array called balls
-let balls = [];
-
-//create a variable to hold your avatar
+let cats = [];
 let me;
+let calicos = [];
 
 
 function setup() {
-  createCanvas(500, 400);
-
-  //make one avatar called me
+  createCanvas(400, 500);
   me = new Avatar(width/2, 300, 3);
-
 }
 
 function draw(){
 	background(220);
-
   me.drawMe();
   me.moveMe();
 
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
+  if (frameCount % 55 == 0) {
+      let  c = new Cat(random(0,width), height-500, -3);
+      cats.push(c);
+      console.log(cats); //print the balls array to the console
     }
 
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
-	  }
-
-}
-
-//avatar class
-class Avatar {
-
-	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
-		    this.x = x;
-    		this.y = y;
-        this.speed = speed;
-	}
-
-	drawMe(){  // draw the running person
-    		stroke("black");
-        strokeWeight(3);
-    		fill("black");
-		    ellipse(this.x,this.y,20,20);
-        line(this.x,this.y, this.x, this.y+40);
-        line(this.x, this.y+40, this.x-20, this.y+60);
-        line(this.x, this.y+40, this.x+10, this.y+50);
-        line(this.x+10, this.y+50, this.x+5, this.y+60);
-        line(this.x, this.y+15, this.x-10, this.y+25);
-        line(this.x-10, this.y+25, this.x+10, this.y+35);
-        noStroke();
-        fill(77,40,0);
-        rect(this.x-13,this.y-27,25,20)
-        fill(153,102,51);
-        rect(this.x-15,this.y-10,30,5);
-
-        // push();
-        // // beginShape();
-        // scale(1/7);
-        // bezier(463, 135, 335, 58, 40, 50, 16, 235);
-        // translate(447,-100);
-        // bezier(463, 135, 372, 364, 128, 241, 16, 235);
-        // translate(-585,250);
-        // bezier(136, 469, 345, 332, 128, 241, 157, 63);
-        // bezier(18, 342, 345, 332, 128, 241, 555, 111);
-        // // endShape();
-        // pop();
-	}
-
-	moveMe(){
-    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
-       this.y -= this.speed;
-    }
-
-    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
-        this.y += this.speed;
-    }
-	}
-
-  die(){
-
+  if (frameCount % 70 == 0) {
+    let  d = new Calico(random(0,width), height-500, -3);
+    calicos.push(d);
+    console.log(calicos);
   }
 
+	// draw all the balls in that array
+	for (let i = 0; i < cats.length; i++) {
+	 	      cats[i].drawCat();
+       	  cats[i].moveCat();
+	  }
+    for (let i = 0; i < calicos.length; i++) {
+  	 	      calicos[i].drawCalico();
+         	  calicos[i].moveCalico();
+  	  }
 }
 
-
 //ball class from which to create new balls with similar properties.
-class Ball {
+class Cat {
 
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed){
@@ -101,34 +48,186 @@ class Ball {
 	}
 
 	// draw a ball on the screen at x,y
-	drawBall(){
-    	noStroke();
-    	fill(123, 131, 189);
-		  ellipse(this.x,this.y,10,10);
-      fill(164, 165, 171);
-      triangle(this.x+27,this.y,this.x+40,this.y+10,this.x+40,this.y-10);
-      fill("gray");
-      ellipse(this.x+17,this.y,26,18);
-      fill("gray");
-      triangle(this.x+10,this.y,this.x+22,this.y-30,this.x+25,this.y);
-      ellipse(this.x+22,this.y-20,7,19);
-      fill(0);
-      ellipse(this.x-1,this.y-1, 2,2);
-      fill(164, 165, 171);
-      triangle(this.x-2,this.y-4,this.x-9,this.y,this.x-9,this.y+4);
+	drawCat(){
+    //whiskers
+    push();
+    stroke(138, 101, 37);
+    strokeWeight(1);
+    line(this.x-8,this.y-25+42,this.x-13,this.y-27+42);
+    line(this.x-8,this.y-23+42,this.x-13,this.y-23+42);
+    line(this.x-8,this.y-21+42,this.x-13,this.y-19+42);
+    line(this.x+8,this.y-25+42,this.x+13,this.y-27+42);
+    line(this.x+8,this.y-23+42,this.x+13,this.y-23+42);
+    line(this.x+8,this.y-21+42,this.x+13,this.y-19+42);
+    pop();
+
+//head + body
+      noStroke();
+    	fill(232, 191, 109);
+		  ellipse(this.x,this.y,25,35);
+      ellipse(this.x,this.y+20,18,18);
+
+//tail
+      push();
+      stroke(232, 191, 109);
+      noFill();
+      strokeWeight(8);
+      translate(-30,-65);
+      bezier(this.x+286/6, this.y+119/6, this.x+264/6, this.y+294/6, this.x+157/6, this.y+138/6, this.x+175/6, this.y+342/6);
+      pop();
+
+//eyes
+      push();
+      noStroke();
+      fill(138, 101, 37);
+      ellipse(this.x-4,this.y+23,4,4);
+      ellipse(this.x+3,this.y+23,4,4);
+      pop();
 	}
 
 	//update the location of the ball, so it moves across the screen
-	moveBall(){
-		this.x = this.x+ this.speed;
-		this.y = this.y+.5;
+	moveCat(){
+    this.x = this.x;
+		this.y = this.y- this.speed;
 	}
 
-	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-    		}
-  	}
+}
+
+class Calico {
+
+	//every ball needs an x value, a y value, and a speed
+	constructor(x,y, speed){
+		this.x = x;
+    this.y = y;
+    this.speed = speed;
+	}
+
+	// draw a ball on the screen at x,y
+	drawCalico(){
+    //whiskers
+    push();
+    stroke(0);
+    strokeWeight(1);
+    line(this.x-8,this.y-25+42,this.x-13,this.y-27+42);
+    line(this.x-8,this.y-23+42,this.x-13,this.y-23+42);
+    line(this.x-8,this.y-21+42,this.x-13,this.y-19+42);
+    line(this.x+8,this.y-25+42,this.x+13,this.y-27+42);
+    line(this.x+8,this.y-23+42,this.x+13,this.y-23+42);
+    line(this.x+8,this.y-21+42,this.x+13,this.y-19+42);
+    pop();
+
+//head + body
+      noStroke();
+    	fill(255);
+		  ellipse(this.x,this.y,25,35);
+      ellipse(this.x,this.y+20,18,18);
+
+//tail
+      push();
+      stroke(255);
+      noFill();
+      strokeWeight(8);
+      translate(-30,-65);
+      bezier(this.x+286/6, this.y+119/6, this.x+264/6, this.y+294/6, this.x+157/6, this.y+138/6, this.x+175/6, this.y+342/6);
+      pop();
+
+//eyes
+      push();
+      noStroke();
+      fill(0);
+      ellipse(this.x-4,this.y+23,4,4);
+      ellipse(this.x+3,this.y+23,4,4);
+      pop();
+
+//spots
+push();
+noStroke();
+fill(163, 163, 163);
+ellipse(this.x-4,this.y,10,8);
+fill(232, 191, 109);
+ellipse(this.x+5,this.y-10,6,6);
+fill(138, 101, 37);
+ellipse(this.x+5,this.y+10,8,10);
+pop();
+	}
+
+	//update the location of the ball, so it moves across the screen
+	moveCalico(){
+    this.x = this.x;
+		this.y = this.y- this.speed;
+	}
+
+}
+
+class Avatar {
+
+	constructor(x,y, speed){ //every avatar needs an x value, a y value, and a speed
+		    this.x = x;
+    		this.y = y;
+        this.speed = speed;
+	}
+
+	drawMe(){  // draw the running person
+    		noStroke();
+        fill(143, 199, 70);
+        //arms
+        ellipse(this.x+20,this.y-12,20, 10);
+        ellipse(this.x+20,this.y+12,20, 10);
+        ellipse(this.x-20,this.y-12,20, 10);
+        ellipse(this.x-20,this.y+12,20, 10);
+        //head
+        ellipse(this.x, this.y-25, 20,25);
+        //tail
+        triangle(this.x, this.y+32, this.x+15, this.y, this.x-15, this.y);
+        //shell
+        fill(45,143,45);
+		    ellipse(this.x,this.y,40,50);
+        //eyes
+        fill("black");
+        ellipse(this.x+8,this.y-30,4,4);
+        ellipse(this.x-8,this.y-30,4,4);
+        //shell design
+        stroke(102,102,51);
+        strokeWeight(1);
+        line(this.x-9,this.y-3.75, this.x-3.75, this.y-11.25);
+        line(this.x+9,this.y+3.75, this.x+3.75, this.y+11.25);
+        line(this.x+9,this.y-3.75, this.x+3.75, this.y-11.25);
+        line(this.x-9,this.y+3.75, this.x-3.75, this.y+11.25);
+        line(this.x-3.75, this.y+11.25, this.x+3.75, this.y+11.25)
+        line(this.x-3.75, this.y-11.25, this.x+3.75, this.y-11.25)
+        line(this.x-9, this.y-3.75, this.x-9, this.y+3.75)
+        line(this.x+9, this.y-3.75, this.x+9, this.y+3.75)
+        line(this.x+9, this.y-3.75, this.x+18, this.y-8)
+        line(this.x-9, this.y-3.75, this.x-18, this.y-8)
+        line(this.x-3.75, this.y-11.25, this.x-7, this.y-23)
+        line(this.x-3.75, this.y+11.25, this.x-7, this.y+23)
+        line(this.x+3.75, this.y-11.25, this.x+7, this.y-23)
+        line(this.x+3.75, this.y+11.25, this.x+7, this.y+23)
+        line(this.x+9, this.y+3.75, this.x+18, this.y+8)
+        line(this.x-9, this.y+3.75, this.x-18, this.y+8)
+
+	}
+
+	moveMe(){
+    if (keyIsDown(UP_ARROW)) { //if you hold the up arrow, move up by speed
+       this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
+        this.y += this.speed;
+      }
+        if(keyIsDown(LEFT_ARROW)){
+     this.x -=this.speed;
+   }
+
+   if(keyIsDown(RIGHT_ARROW)){
+     this.x +=this.speed;
+
+    }
+	}
+
+  die(){
+
+  }
 
 }
